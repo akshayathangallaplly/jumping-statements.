96314b2da8c54fe70726012d71b3f2e036207e05
akshayathangallapally <ta7440360@gmail.com>
2025-02-27 09:13:24 +0530

    Add files via upload

---
+++ Jumping statements.py
@@ -0,0 +1,25 @@
+'''
+Pass
+break
+continue
+'''
+if True:
+    pass
+
+#for loop break 
+for i in "pythonlife":
+    if i=='l':
+        break
+    print(i)
+
+#for loop continue
+for i in "pythonlife":
+    if i=='e':
+        continue
+    print(i)
+
+'''
+pass-ignore
+break--terminate
+continue--skip(current iteration)
+'''
